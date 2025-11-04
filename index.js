b26611afbf607fc22deb4531b3271da0f0729cd7
Yvette Hernandez <yhern006@users.noreply.github.com>
2025-11-03 21:16:20 -0800

    added radio highlighting

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import { catsData } from "./data.js"
 
 const emotionRadiosEl = document.getElementById('emotion-radios')
 
+emotionRadiosEl.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+
+    for(let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats) {
